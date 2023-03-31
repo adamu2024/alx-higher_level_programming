9fa6f3bdc17d78146052941cee26369df771c2ba
adamu2024 <jubril.adamu2024@gmail.com>
2023-03-31 08:51:58 -0700

    task5

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i == 99:
-        print(i)
-else:
-    print(f"{}".format('0' + str(i) if i < 10 else i), end=", ")
+    if int(i / 10) != i % 10 and int(i / 10) < i % 10:
+        print("{}{}".format(int(i / 10), i % 10), end="")
+        if (i != 89):
+            print(", ", end="")
+        
+print("")
