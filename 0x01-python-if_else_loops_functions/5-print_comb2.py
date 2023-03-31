c70b4ab9914a3ca9c6af3995a339bcc53e52cfb5
adamu2024 <jubril.adamu2024@gmail.com>
2023-03-31 08:22:38 -0700

    task5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i == 99:
-        print(i)
-else:
-    print(f"{}".format('0' + str(i) if i < 10 else i), end=", ")
+for i in range(0, 100 1):
+    print("{}" .format('0' + str(i) if i < 10 else i), end=", ")
